c6cc5f655c5bcb95b016723c694facebae62d7ab
apatiga <128877342+apatiga@users.noreply.github.com>
2023-06-21 23:07:22 -0700

    fixed deployment issure and routing issue in api routes. Finsihed README.md file. put in delete request portionin api routes to be able to delete notes.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,39 +1,39 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const fs =require ("fs");
+// const {title} = require("../Develop/db/db.json");
 
 
 
 router.get('/api/notes', async (req, res) => {
-    const dbJson = await JSON.parse(fs.readFileSync('db/db.json"',"utf8"),
-    res.json(dbJson));
+    const dbJson = await JSON.parse(fs.readFileSync('./Develop/db/db.json',"utf8"));
+    res.json(dbJson);
 });
 
 router.post('/api/notes',(req, res) => {
-    const dbJson = JSON.parse(fs.readFileSync('db/db.json"',"utf8"));
-    const newFeedbaack = {
-        title: req.nody.title,
-        text: req,body,text,
+    const dbJson = JSON.parse(fs.readFileSync('./Develop/db/db.json',"utf8"));
+    const newFeedback = {
+        title: req.body.title,
+        text: req.body.text,
         id: uuidv4(),
     };
     dbJson.push(newFeedback);
-    fs.writeFileSynce("db/db.json",JSON.stringify(dbJson));
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(dbJson));
     res.json(dbJson);
 });
 
 // delete request portion 
 
-router.post('/api/notes',(req, res) => {
-    const dbJson = JSON.parse(fs.readFileSync('db/db.json"',"utf8"));
+router.delete('/api/notes/:id', (req, res) => {
+    let data = fs.readFileSync('./Develop/db/db.json',"utf8");
     const dataJSON = JSON.parse(data);
-    const NewNotes = dataJSOn.filter((note) => {
+    const newNotes = dataJSON.filter((note) => {
         return note.id !== req.params.id;
     });
-    fs.writeFileSynce("db/db.json",JSON.stringify(newNotes));
+    fs.writeFileSync("./Develop/db/db.json",JSON.stringify(newNotes))
     res.json("Note deleted.");
 });
 
     module.exports = router;
 
-
-
+ 
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const api_routes = require('./routes/api-routes');
 const html_routes = require('./routes/html-routes');
 
-app.use(express.static('public'));
+app.use(express.static("./Develop/public"))
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 // Use apiRoutes
